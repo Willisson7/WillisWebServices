03fdb48c042bf2c3c4f1cfbaee81dd525784f11b
[Grant Willis] <[thegwillis.gw@gmail.com]>
2024-05-11 14:02:46 -0700

    Wrote deleteContact function

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -60,7 +60,26 @@ const updateContact = async (req, res, next) => {
   }
 };
 
-const deleteContact = async (req, res, next) => {};
+const deleteContact = async (req, res, next) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .deleteOne({ _id: userId });
+    if (result.deleteCount === 0) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+
+    res.status(200).end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
 module.exports = {
   getAll,
   getSingle,
